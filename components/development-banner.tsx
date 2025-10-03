4286944b809766a6ab6d78c89b930663ad3c2cec
pcodefromarch <primecoder8@gmail.com>
2025-10-03 17:08:16 +0530

    add test banner

--- components/development-banner.tsx
+++ components/development-banner.tsx
@@ -1,12 +1,13 @@
 import { AlertTriangle } from "lucide-react"
-
+// TODO: uncomment banner line
 export default function DevelopmentBanner() {
   return (
     <div className="bg-amber-500 text-amber-900 px-4 py-3 text-center font-medium">
       <div className="flex items-center justify-center gap-2">
         <AlertTriangle className="h-5 w-5" />
         <span>
-          🚧 This website is under heavy development. Some features may be incomplete or change frequently. 🚧
+          {/* This website is under heavy development. Some features may be incomplete or change frequently.  */}
+          This is Test deployment backend has'nt be deployed nothing will work
         </span>
         <AlertTriangle className="h-5 w-5" />
       </div>
